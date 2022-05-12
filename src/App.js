a73f4e0c5a75f561ee30d9781a66df16eda839aa
dimas <dimmaa9@gmail.com>
2022-05-12 22:49:48 +0300

    arina privet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import {useWeb3React} from "@web3-react/core";
+import {connectors} from "./connectors";
+import {useEffect, useState} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {library, chainId, account, activate, deactivate, active} = useWeb3React();
+
+    useEffect(() => {
+        const provider = window.localStorage.getItem("provider");
+        if (provider) activate(connectors[provider]);
+    }, []);
+
+    const setProvider = (type) => {
+        window.localStorage.setItem("provider", type);
+    };
+
+    return (
+        <>
+            <div className="wallet_items">
+                <a onClick={() => {
+                    activate(connectors.injected);
+                    setProvider("injected");
+                }}>
+                    <div className="wallet_item">
+                        <img src="img/metamask.png" alt=""/>
+                        <p>Metamask</p>
+                    </div>
+                </a>
+                <a onClick={
+                    () => {
+                        activate(connectors.walletConnect);
+                        setProvider("walletConnect");
+                    }}>
+                    <div className="wallet_item">
+                        <img src="img/wallet_connect.png" alt=""/>
+                        <p>WalletConnect</p>
+                    </div>
+                </a>
+                <a onClick={() => {
+                    activate(connectors.coinbaseWallet);
+                    setProvider("coinbaseWallet");
+                }}>
+                    <div className="wallet_item">
+                        <img src="img/coinbase.png" alt=""/>
+                        <p>Coinbase</p>
+                    </div>
+                </a>
+            </div>
+            <div className="wallet_more">
+                <img src="img/information.png" alt=""/>
+                <a href="/">Learn how to connect</a>
+            </div>
+        </>
+    );
 }
 
 export default App;
